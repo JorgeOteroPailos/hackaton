78d73d6a17503fd1951835007f9eeadb54b6031c
manuramazz <159024669+manuramazz@users.noreply.github.com>
2025-02-22 17:58:49 +0100

    Update server.py session start

--- server.py
+++ server.py
@@ -5,19 +5,19 @@ from langchain_ollama import OllamaLLM
 from langchain_ollama.chat_models import ChatOllama
 from langchain.prompts import PromptTemplate
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_huggingface import HuggingFaceEmbeddings  # Modelo de embeddings sin necesidad de clave API
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import LLMChain
 from fastapi import FastAPI, HTTPException
 import math
 
 server = FastAPI()
 
-#Estructura que recibe en la conexión
+#Estructura que recibe por la conexión http
 class InsertarStruct(BaseModel):
     clave: str
     frase_competencia: str
 
-# Definir la estructura de los datos que esperamos del modelo
+# Definir la estructura de los datos que esperamos del modelo llama3.2
 class CompetenciaResponse(BaseModel):
     item: str = Field(description="La competencia aprendida referida en la frase")
 
@@ -36,20 +36,17 @@ def get_trabajadores():
     return trabajadores
 
 
-#CODIGO INSERTAR COMPETENCIAS
-# Función para insertar o actualizar competencia en la base de datos
-def insertar_o_actualizar_competencia(clave, competencia):
+# Función para insertar competencias en la base de datos
+def insertar_competencia(clave, competencia):
     conn = sqlite3.connect('empresa.db')
     cursor = conn.cursor()
 
-    # Buscar si ya existe un trabajador con el mismo nombre
+    # Buscar todas las competencias de ese trabajador 
     cursor.execute("SELECT competencias FROM trabajadores WHERE clave = ?", (clave,))
     trabajador = cursor.fetchone()
 
     if trabajador:
-        # Si ya existe, actualizar la competencia agregando la nueva
         competencias_existentes = trabajador[0]
-        # Comprobar si la competencia no está en las competencias existentes
         if competencia not in competencias_existentes:
             competencias_actualizadas = competencias_existentes + ", " + competencia
             cursor.execute("UPDATE trabajadores SET competencias = ? WHERE clave = ?", (competencias_actualizadas, clave))
@@ -57,34 +54,30 @@ def insertar_o_actualizar_competencia(clave, competencia):
     conn.commit()
     conn.close()
 
+
 # Configuración del modelo Ollama
 llm = ChatOllama(model = "llama3.2:3b", temperature = 0)
 
 
-    #CODIGO CONSULTAR COMPETENCIAS
-    # Función para vectorizar a los trabajadores y la consulta
+ 
+# Función para vectorizar a los trabajadores y la consulta
 def vectorize_text(texts, embeddings_model):
     return embeddings_model.embed_documents(texts)
 
-# Función para encontrar los trabajadores más similares utilizando embeddings
+
+# Función para encontrar los trabajadores más similares utilizando embeddings, devolviendo el top n más coincidente
 def find_top_matching_workers_with_embeddings(query, trabajadores, top_n=5, embeddings_model=None):
-    # Vectoriza la consulta
     query_embedding = embeddings_model.embed_query(query)
     
-    # Crear las competencias y nombres de los trabajadores en formato de texto
-    #trabajadores_texto = [f"{nombre}: {competencias}" for nombre, competencias in trabajadores]
     competencias_texto = [f"{competencias}" for competencias in trabajadores]
     
-    # Vectoriza las competencias de los trabajadores
     worker_embeddings = vectorize_text(competencias_texto, embeddings_model)
 
-    # Calcular similitud de coseno entre la consulta y los trabajadores
     scores = []
     for i, (nombre, competencias) in enumerate(trabajadores):
         score = cosine_similarity(query_embedding, worker_embeddings[i])
         scores.append((nombre, competencias, score))
 
-    # Ordenar por mayor similitud y tomar los X mejores
     scores.sort(key=lambda x: x[2], reverse=True)
     
     return scores[:top_n]
@@ -98,17 +91,32 @@ def cosine_similarity(vec1, vec2):
         return 0
     return dot_product / (norm_vec1 * norm_vec2)
 
-# Configuración del modelo de embeddings sin necesidad de API key
+# Configuración del modelo de embeddings para vectorizar las listas de competencias de cada trabajador
 embeddings_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
+#busca un trabajador con la clave proporcionada y en caso de existir devuelve su nombre
+def comprobar_inicio_sesion(clave):
+    conn = sqlite3.connect('empresa.db')
+    cursor = conn.cursor()
 
-#Endpoint para consultar trabajadores
+    cursor.execute("SELECT nombre FROM trabajadores WHERE clave = ?", (clave,))
+    
+    resultado = cursor.fetchone()
+    conn.close()
+
+    if resultado:
+        return resultado[0]
+    else:
+        return "0"  
+
+#Endpoint para consultar trabajadores con conocimientos sobre un tema específico
 @server.post("/consultar")
 def consultar_trabajadores(query: str):
     trabajadores = get_trabajadores()
 
     resultados = find_top_matching_workers_with_embeddings(query, trabajadores, top_n,embeddings_model)
-    # Filtrar trabajadores con score < min_similarity
+
+    # Filtrar trabajadores que superan el umbral de similaridad
     filtered_workers = [(nombre, competencias, score) for nombre, competencias, score in matching_workers if score >= min_similarity]
 
     if not filtered_workers:
@@ -117,15 +125,20 @@ def consultar_trabajadores(query: str):
     return [{"nombre": nombre, "competencias": competencias, "similitud": round(score, 2)} for nombre, competencias, score in filtered_workers]
 
 
-#  Endpoint para agregar competencia
+#  Endpoint para agregar una competencia de un trabajador
 @server.post("/insertar")
 def agregar_competencia(request: InsertarStruct):
     model_with_structure = llm.with_structured_output(CompetenciaResponse)
     structured_output = model_with_structure.invoke(request.frase_competencia)
     competencia = structured_output.item
 
-    insertar_o_actualizar_competencia(request.clave, competencia)
-    return {"mensaje": f"Competencia '{competencia}' agregada a {request.nombre}"}
+    insertar_competencia(request.clave, competencia)
+    return {f"Competencia '{competencia}' agregada a {request.nombre}"}
+
+@server.post("/sesion")
+def inicio_sesion(clave: str):
+    value = comprobar_inicio_sesion(clave)
+
 
 
 # Ejecutar servidor
