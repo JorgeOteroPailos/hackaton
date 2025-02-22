56380a6eb0b795ed79b77c1e074b623ac0e9657b
xocass <joaquin.quintas@rai.usc.es>
2025-02-22 19:16:01 +0100

    inicio sesion funcional

--- src/main/java/com/hackudc/competenciapp/CInicioSesion.java
+++ src/main/java/com/hackudc/competenciapp/CInicioSesion.java
@@ -1,29 +1,54 @@
 package com.hackudc.competenciapp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CInicioSesion {
-    private Main main;
+    private Stage stage;
     @FXML
     private TextField clave;
     @FXML
     private Button enter;
 
-    public void initCInicioSesion(Main main) {
-        this.main = main;
+    public void initCInicioSesion(Stage stage){
+        this.stage = stage;
     }
+
     @FXML
     private void verificarUsuario() {
-        main.verificarUsuario(clave.getText());
+        String aux = Utils.verificarUsuario(clave.getText());
+        if(!aux.equals("false")){
+            abrirCPrincipal(aux);
+        }else {
+
+        }
     }
     @FXML
     public void manejadorTeclas(KeyEvent event) {
         if (event.getCode() == KeyCode.ENTER) {
             verificarUsuario();
+        }
+    }
+
+    public void abrirCPrincipal(String nombre){
+        try {
+            //Cargar la ventana de inicio de sesión
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Principal.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 540, 345);
+            CPrincipal controller = fxmlLoader.getController();
+            stage.setTitle(nombre);
+            stage.setScene(scene);
+            stage.show();
+        }catch (IOException e){
+
         }
     }
 }
---
+++ src/main/java/com/hackudc/competenciapp/Utils.java
@@ -0,0 +1,137 @@
+package com.hackudc.competenciapp;
+
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class Utils {
+    private static HttpURLConnection conexion;
+    static final URL CONSULTAS;
+    static final URL INSERTAR_DATOS;
+    static final URL INICIO_SESION;
+
+    static {
+        try {
+            CONSULTAS = new URL("http://localhost:8000/consultar");
+            INSERTAR_DATOS = new URL("http://localhost:8000/insertar");
+            INICIO_SESION = new URL("http://localhost:8000/sesion");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Función para verificar el usuario
+    public static String verificarUsuario(String clave){
+        setConexionInicioSesion();
+        StringBuilder jsonBuilder = new StringBuilder("{\"clave\": \"" + clave + "\"}");
+        return enviarMensaje(jsonBuilder);
+    }
+
+    private static String enviarMensaje(StringBuilder jsonBuilder) {
+        StringBuilder respuesta = new StringBuilder();
+        try (OutputStream os = conexion.getOutputStream()) {
+            byte[] input = jsonBuilder.toString().getBytes(StandardCharsets.UTF_8);
+            os.write(input);
+            System.out.println("ENVIADO: " + jsonBuilder.toString());
+            int rc = conexion.getResponseCode();
+            if (rc != 200) {
+                System.out.println("ERROR: rc=" + rc);
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(conexion.getInputStream(), StandardCharsets.UTF_8))) {
+            respuesta = new StringBuilder(new String());
+            String aux;
+            while ((aux = br.readLine()) != null) {
+                respuesta.append(aux.trim());
+            }
+            System.out.println("Respuesta del servidor: " + respuesta);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return respuesta.toString();
+    }
+
+    public static String hacerConsulta(String consulta){
+        setConexionConsulta();
+        StringBuilder jsonBuilder = new StringBuilder("{\"query\":"+consulta+"}");
+        return enviarMensaje(jsonBuilder);
+    }
+
+    public static String insertarDatos(String datos){
+        setConexionInsertarDatos();
+        StringBuilder jsonBuilder = new StringBuilder("{\"query\":"+datos+"}");
+        return enviarMensaje(jsonBuilder);
+    }
+
+    public static void setConexionConsulta() {
+        try {
+            conexion = (HttpURLConnection) CONSULTAS.openConnection();
+            conexion.setRequestMethod("POST");
+            conexion.setRequestProperty("Content-Type", "application/json");
+            conexion.setDoOutput(true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setConexionInicioSesion() {
+        try {
+            conexion = (HttpURLConnection) INICIO_SESION.openConnection();
+            conexion.setRequestMethod("POST");
+            conexion.setRequestProperty("Content-Type", "application/json");
+            conexion.setDoOutput(true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setConexionInsertarDatos() {
+        try {
+            conexion = (HttpURLConnection) INSERTAR_DATOS.openConnection();
+            conexion.setRequestMethod("POST");
+            conexion.setRequestProperty("Content-Type", "application/json");
+            conexion.setDoOutput(true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+        public static void showPopup (String title, String message, Alert.AlertType type){
+            // Crear unha alerta
+            Alert alert = new Alert(type);
+
+            // Configurar o título e contido
+            alert.setTitle(title);
+            alert.setHeaderText(null); // Opcional, podes engadir un subtítulo
+            alert.setContentText(message);
+
+            // Mostrar o popup e esperar que o usuario o peche
+            alert.showAndWait();
+        }
+
+        public static void showPopup (Exception e){
+            // Crear unha alerta
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+
+            // Configurar o título e contido
+            alert.setTitle("Error");
+            alert.setHeaderText(null); // Opcional, podes engadir un subtítulo
+            alert.setContentText(e.getMessage());
+
+            // Mostrar o popup e esperar que o usuario o peche
+            alert.showAndWait();
+        }
+}
