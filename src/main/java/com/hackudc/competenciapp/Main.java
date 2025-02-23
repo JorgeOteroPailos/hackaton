e9f8136864f4f85880994087570baad373c8ca10
xocass <joaquin.quintas@rai.usc.es>
2025-02-23 02:07:44 +0100

    avances de la noche

--- src/main/java/com/hackudc/competenciapp/Main.java
+++ src/main/java/com/hackudc/competenciapp/Main.java
@@ -2,12 +2,6 @@ package com.hackudc.competenciapp;
 
 import javafx.application.Application;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -15,23 +9,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
-    //Variables para la conexión
-    private HttpURLConnection conexion;
-    private URL consultas;
-    private URL insertarDatos;
-    private URL inicioSesion;
-
-    //Variables para guardar datos
-    private String clave;
-    private String nombre;
-
+    
     @Override
     public void start(Stage stage) throws IOException {
-        //Asignar la url del servidor
-        consultas = new URL("http://localhost:8000/consultar");
-        insertarDatos = new URL("http://localhost:8000/insertar");
-        inicioSesion = new URL("http://localhost:8000/sesion");
-
         //Cargar la ventana de inicio de sesión
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("InicioSesion.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 260, 125);
@@ -39,11 +19,12 @@ public class Main extends Application {
         controller.initCInicioSesion(stage);
         stage.setTitle("Inicio Sesión");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
-    
+
 }
